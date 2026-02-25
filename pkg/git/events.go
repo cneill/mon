ee3516b751ef9183e6b25caec07e5f8e8d1d0cd4
Charles Neill <1749665+cneill@users.noreply.github.com>
2026-02-25 00:19:01 -0600

    Adding push

--- pkg/git/events.go
+++ pkg/git/events.go
@@ -3,8 +3,9 @@ package git
 type EventType string
 
 const (
-	EventTypeUnknown   EventType = "unknown"
-	EventTypeNewCommit EventType = "new commit"
+	EventTypeUnknown    EventType = "unknown"
+	EventTypeNewCommit  EventType = "new commit"
+	EventTypeCommitPush EventType = "commit push"
 )
 
 type Event struct {
